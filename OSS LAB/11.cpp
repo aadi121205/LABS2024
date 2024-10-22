e8d7baae3ab1d05e61df8dbcd57cd97bcecd3f43
aadi <aaditya12120205@gmail.com>
2024-10-22 10:26:12 +0530

    update

--- OSS LAB/11.cpp
+++ OSS LAB/11.cpp
@@ -1,1 +1,98 @@
 // WAP to perform string operations using operator overloading in C++i, = String Copy l. =, Equality ii. Concatenation
+
+#include <iostream>
+
+using namespace std;
+
+class String {
+    char *str;
+    int len;
+
+public:
+    String() {
+        str = new char[1];
+        str[0] = '\0';
+        len = 0;
+    }
+
+    String(const char *s) {
+        len = 0;
+        while (s[len] != '\0') {
+            len++;
+        }
+        str = new char[len + 1];
+        for (int i = 0; i < len; i++) {
+            str[i] = s[i];
+        }
+        str[len] = '\0';
+    }
+
+    String(const String &s) {
+        len = s.len;
+        str = new char[len + 1];
+        for (int i = 0; i < len; i++) {
+            str[i] = s.str[i];
+        }
+        str[len] = '\0';
+    }
+
+    ~String() {
+        delete[] str;
+    }
+
+    void display() {
+        cout << str << endl;
+    }
+
+    String operator=(const String &s) {
+        len = s.len;
+        delete[] str;
+        str = new char[len + 1];
+        for (int i = 0; i < len; i++) {
+            str[i] = s.str[i];
+        }
+        str[len] = '\0';
+        return *this;
+    }
+
+    String operator+(const String &s) {
+        String temp;
+        temp.len = len + s.len;
+        delete[] temp.str;
+        temp.str = new char[temp.len + 1];
+        for (int i = 0; i < len; i++) {
+            temp.str[i] = str[i];
+        }
+        for (int i = 0; i < s.len; i++) {
+            temp.str[len + i] = s.str[i];
+        }
+        temp.str[temp.len] = '\0';
+        return temp;
+    }
+
+    bool operator==(const String &s) {
+        if (len != s.len) {
+            return false;
+        }
+        for (int i = 0; i < len; i++) {
+            if (str[i] != s.str[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
+int main() {
+    String s1("Hello"), s2("World"), s3;
+
+    s3 = s1 + s2;
+    s3.display();
+
+    cout << (s1 == s2) << endl;
+    cout << (s1 == s1) << endl;
+
+    return 0;
+}
+
+// By Aaditya Bhatia
--- OSS LAB/13.cpp
+++ OSS LAB/13.cpp
@@ -1,1 +1,61 @@
 // Write a program to aceept the student detail such as name and 3 different marks get_data0 method and display the name and average of marks using displayO method. Define a friend class for calculating the average of marks using the method mark_ avg()
+
+#include <iostream>
+using namespace std;
+
+class Student; // Forward declaration
+
+class Student {
+private:
+    string name;
+    int marks[3];
+
+public:
+    // Method to get student details
+    void get_data() {
+        cout << "Enter student name: ";
+        cin >> name;
+        cout << "Enter marks in 3 subjects: ";
+        for (int i = 0; i < 3; i++) {
+            cin >> marks[i];
+        }
+    }
+
+    // Method to calculate average marks
+
+    float mark_avg() {
+        int sum = 0;
+        for (int i = 0; i < 3; i++) {
+            sum += marks[i];
+        }
+        return static_cast<float>(sum) / 3;
+    }
+
+    // Method to display student details
+    void display() {
+        cout << "Name: " << name << endl;
+        cout << "Average marks: " << mark_avg() << endl;
+    }
+
+    // Friend class declaration
+    friend class FriendClass;
+};
+
+class FriendClass {
+public:
+    // Friend function to calculate average marks
+    static float mark_avg(Student s) {
+        int sum = 0;
+        for (int i = 0; i < 3; i++) {
+            sum += s.marks[i];
+        }
+        return static_cast<float>(sum) / 3;
+    }
+};
+
+int main() {
+    Student s;
+    s.get_data();
+    s.display();
+    return 0;
+}
